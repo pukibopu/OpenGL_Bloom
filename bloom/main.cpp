018e5b13b8f5a6cd738e374f8281c730fdd86ed6
jinjo <creationer625@outlook.com>
2022-11-18 21:44:13 +0800

    initial project

--- bloom/main.cpp
+++ bloom/main.cpp
@@ -4,11 +4,170 @@
 //
 //  Created by ShuYang Yan on 2022/11/17.
 //
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <shader.hpp>
+#include <camera.hpp>
+#include <model.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
 
+const int &&SCR_WIDTH=800;
+const int &&SCR_HEIGHT=600;
+
+void framebuffer_size_callback(GLFWwindow *window,int width,int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+unsigned int loadTexture(const char *path, bool gammaCorrection);
+void renderQuad();
+void renderCube();
+bool bloom = true;
+bool bloomKeyPressed = false;
+float exposure = 1.0f;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));
+float lastX = (float)SCR_WIDTH / 2.0;
+float lastY = (float)SCR_HEIGHT / 2.0;
+bool firstMouse = true;
+
+float deltaTime=0.0f;
+float lastTime=0.0f;
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+#endif
+    
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "test", NULL, NULL);
+    if(!window){
+        std::cout<<"ERROR TO CREATE WINDOW"<<std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+        
+    }
+    
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    
+    
+    
+    
+    if(!gladLoadGLLoader(GLADloadproc(glfwGetProcAddress))){
+        std::cout<<"ERROR TO INTIALIZE GLAD"<<std::endl;
+        return -1;
+    }
+    
+    glEnable(GL_DEPTH_TEST);
+    
+    
+    while(!glfwWindowShouldClose(window)){
+        
+        deltaTime=static_cast<float>(glfwGetTime())-lastTime;
+        lastTime=static_cast<float>(glfwGetTime());
+        processInput(window);
+        
+        
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.1f,0.1f,0.1f,1.0f);
+        
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        
+    }
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window,int width,int height){
+    glViewport(0,0,width,height);
+}
+
+
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && !bloomKeyPressed)
+    {
+        bloom = !bloom;
+        bloomKeyPressed = true;
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE)
+    {
+        bloomKeyPressed = false;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+    {
+        if (exposure > 0.0f)
+            exposure -= 0.001f;
+        else
+            exposure = 0.0f;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+    {
+        exposure += 0.001f;
+    }
+}
+
+
+unsigned int loadTexture(const char *path, bool gammaCorrection){
+    GLuint textureId;
+    glGenTextures(1,&textureId);
+    
+    
+    return textureId;
 }
